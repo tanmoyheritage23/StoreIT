b5b11acea85fe188d0260be59531cd8e33e9596b
Tanmoy Saha <89009776+tanmoyheritage23@users.noreply.github.com>
2025-02-11 23:59:32 +0530

    Fixing percentage issue in the Dashboard

--- lib/utils.ts
+++ lib/utils.ts
@@ -25,7 +25,7 @@ export const convertFileSize = (sizeInBytes: number, digits?: number) => {
 };
 
 export const calculatePercentage = (sizeInBytes: number) => {
-  const totalSizeInBytes = 2 * 1024 * 1024 * 1024; // 2GB in bytes
+  const totalSizeInBytes = 150 * 1024 * 1024 * 1024; // 150GB in bytes
   const percentage = (sizeInBytes / totalSizeInBytes) * 100;
   return Number(percentage.toFixed(2));
 };
